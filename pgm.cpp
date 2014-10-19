8086afb9a6c20b22590f58a60313321240b42160
victorJB <slayerhd363@gmail.com>
2014-10-18 20:25:17 -0500

    Herencia Implementada

---
+++ pgm.cpp
@@ -0,0 +1,1 @@
+#include <pgm.h>
---
+++ pgm.h
@@ -0,0 +1,18 @@
+#ifndef PGM_H
+#define PGM_H
+
+#include <matrix.h>
+#include <iostream>
+
+using namespace std;
+
+class PGM : public Matrix
+{
+private:
+
+
+public:
+
+};
+
+#endif // PGM_H
